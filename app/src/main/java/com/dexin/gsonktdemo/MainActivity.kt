7df6109a7e8010cb459c5fc2880d2ec08ecde963
Ting <ticccccc@163.com>
2018-04-19 18:02:33 +0800

    添加测试用数据

--- app/src/main/java/com/dexin/gsonktdemo/MainActivity.kt
+++ app/src/main/java/com/dexin/gsonktdemo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.dexin.gsonktdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var result: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initData()
+    }
+
+    private fun initData() {
+        val ist = assets.open("data.json")
+        val len = ist.available()
+        val bytes = ByteArray(len)
+        val read = ist.read(bytes)
+        ist.close()
+        result = String(bytes)
+        Log.e("main", result)
     }
 }
